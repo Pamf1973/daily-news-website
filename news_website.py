f3e43e58c471416566fe10596cb325d2cbb5d4b0
Pamf1973 <pedroprousa2023@gmail.com>
2025-05-23 21:20:38 -0400

    fix: correct indentation under main

--- news_website.py
+++ news_website.py
@@ -100,6 +100,6 @@ def home():
 
 if __name__ == "__main__":
     if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
